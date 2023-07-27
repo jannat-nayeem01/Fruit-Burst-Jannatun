dcb1f705807b34ba9998e1e2a39ad80b4d16ed72
Jannatun Nayeem <jannat.nayeem01@gmail.com>
2023-07-27 22:07:39 +0600

    team section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,13 @@ module.exports = {
     plugins: [],
   }
 
-  
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/images/team.png')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
